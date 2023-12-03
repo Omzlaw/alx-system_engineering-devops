0adddd8d092e02f775f549bc0c7f33d231506fd1
Omeiza Alabi <omeizaalabi@gmail.com>
2023-12-03 19:01:11 +0100

    Update file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
-# Installs Flask
+# Installs flask
 
-class flask {
-  package { 'python3-pip':
-    ensure => 'latest',
-  }
-
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask',
-    path    => ['/usr/bin'],
-    require => Package['python3-pip'],
-  }
-}
-
-class { 'flask':
-  ensure => '2.1.0',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
