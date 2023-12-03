3ee81bca95d7f1c24d520b4b3aa727c949492fba
Omeiza Alabi <omeizaalabi@gmail.com>
2023-12-03 20:20:27 +0100

    Web servers

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+class { 'nginx':
+  manage_repo => true,
+  package_source => 'nginx-mainline',
+}
+
+package { 'curl': ensure => installed }
+
+nginx::resource::server { 'default':
+  listen_port => 80,
+  location '/' {
+    root => '/var/www/html',
+  },
+  location '/redirect_me' {
+    return 301 'https://www.youtube.com/watch?v=QH2-TGUlwu4';
+  },
+}
+
+file { '/var/www/html/index.html':
+  ensure => present,
+  content => 'Hello World!',
+}
